e220aa93b6fb0b7e8eb52ec4aa1bff32c1a480c4
jungyu_lee <jungyu.lee1@cj.net>
2022-10-27 14:16:57 +0900

    filter

--- dateTest/DateTest.js
+++ dateTest/DateTest.js
@@ -11,3 +11,10 @@ console.log(date);
 console.log(secondDate);
 
 console.log(realDate);
+
+
+const array = [1, 2, 3, 4, 5, 6];
+
+let arrayResult = array.filter(i => i % 2 == 0);
+
+console.log(arrayResult);
